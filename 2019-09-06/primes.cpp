6f5b3b9b6087390eb5bd13e260aab71c799eb618
Anyelo Leonardy Alvarez Baquero <anlalvarezba@darkstar.org>
2019-09-06 12:43:31 -0500

    Archivo que suma los números primos

---
+++ 2019-09-06/primes.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdio>
+int isprime(int a);
+int sumprimes (int imin, int imax);
+
+int main(void)
+{
+    std::printf("isprime(%d) = %d\n", 7, isprime(7) );
+    std::printf("sumprimes(%d,%d)=%d\n", 50, 100, sumprimes(50, 100));
+  return 0;
+}
+
+int isprime(int a){
+
+  for (int ii=2; ii< a/2; ++i){
+    if (a%ii = 0) return 0;
+  }
+  return 1;
+		   }
+
+int sumprime (int imin, int imax){
+  int sum =0;
+  for (int ii=imin; ii <= imax; +ii){
+    if (isprime(ii=))
+      sum +=ii;
+  }
+  return sum;
+}
