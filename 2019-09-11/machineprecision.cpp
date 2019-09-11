6b23eae238cc87ace333a1c68654eb8eb721a942
Anyelo Leonardy Alvarez Baquero <anlalvarezba@darkstar.org>
2019-09-11 12:08:37 -0500

    Programa que me determina la precision de la maquina

---
+++ 2019-09-11/machineprecision.cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+
+int main(void){
+  
+  float eps=1.0;
+  float one=1.0;
+for (int ii=0;ii<1000; ++ii){
+  eps /=2.0;
+  one =1.0 + eps;
+  std::printf("%10d %24.16e %14.16e\n", ii, one, eps);
+ }
+ 
+ return 0; 
+
+ }
